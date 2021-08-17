7d2536826c4f2c9ffc081ac72de8a1125177aede
Pinaksh <pinakshglobal@gmail.com>
2021-08-18 00:27:15 +0530

    End of Section 2

---
+++ API/Data/Migrations/AddUsers_SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Abhay");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Shweta");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Aaradhya");
